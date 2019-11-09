037aa1785cd2fb57964793dd60648a9fdb90c28e
Afonso Muralha <afonso.muralha@tecnico.ulisboa.pt>
2019-11-09 15:33:41 +0000

    cashback script improvements

--- src/cashback.py
+++ src/cashback.py
@@ -16,9 +16,32 @@ import json
 
 '''
 
+
+
 #get info from the database
+
+#placeholders for the variables
+orderdict = {
+    'items': [
+        {'name':'coffee', 'qty':2, 'unit_price':1.5, 'total_price': 3},
+        {'name':'coffee', 'qty':1, 'unit_price':1.5, 'total_price': 1.5}
+    ],
+
+    'ammounts': {
+        'total' : 3.5,
+        'payed' : 5,
+        'change':{
+            'eur':1.5,
+            'satishi':1234
+        }
+    }
+}
+
 
 #generate lightning request
 
 #compile everything on a json
+
+orderjson = json.dumps(orderdict)
+
 
