09a492014211045128de4249a61a05eaf537a0de
Afonso Muralha <afonso.muralha@tecnico.ulisboa.pt>
2019-11-09 14:37:44 +0000

    Preliminary repo structure; added initial python scripts for the recpit generator, added a placeholder for a receipt demo.

---
+++ src/cashback.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import mysql.connector
+import json
+
+'''
+{
+    'items': [
+        ['name':'coffee', 'qty':'2', 'unit_price':'1', 'total_price':'1']
+    ]
+
+    'ammounts': [
+        ['']
+    ]
+}
+
+'''
+
+#get info from the database
+
+#generate lightning request
+
+#compile everything on a json
+
