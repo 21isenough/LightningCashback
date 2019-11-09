0219c19c6637a0e06541677953ceaf9aa5214157
21isenough <39227503+21isenough@users.noreply.github.com>
2019-11-09 15:45:48 +0000

    add calculations

--- src/lightningqr.py
+++ src/lightningqr.py
@@ -1,24 +1,50 @@
 #!/usr/bin/python
 
+from config import *
+import requests, json, sys
+
 ### Pass the amount in EUR to this funtions to receive LNURL string back
 
 def generate_lnurl(amt):
 
-    request = requests.get('https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCEUR')
+    request = requests.get(PRICEAPI)
     json_data = json.loads(request.text)
     price = json_data['last']
 
-    print(price)
+    satspereuro = round((1 / price) * 100000000)
+    satamount = round(satspereuro * amt)
 
-    data = {
-            'satoshis': str(round(amt)),
-    }
+    print("Sats per Euro = " + str(satspereuro))
+    print("Sats Total = " + str(satamount))
 
-    response = requests.post(
-        APIURL,
-        auth=(USER, PASS),
-        data=json.dumps(data),
-        )
 
-    response = json.loads(response.text)
-    print(response['lnurl'])
+    #data = {
+    #        'satoshis': str(round(amt)),
+    #}
+
+    #response = requests.post(
+    #    APIURL,
+    #    auth=(USER, PASS),
+    #    data=json.dumps(data),
+    #    )
+
+    #response = json.loads(response.text)
+    #print(response['lnurl'])
+
+    response = json.dumps({'satoshis': satamount, 'lnurl': 'test'})
+
+
+    print(response)
+
+    #return json.dumps({'satoshis': satamount, 'lnurl': response['lnurl']})
+
+    #{
+   #'satoshis': satamount,
+   #'lnurl': response['lnurl'],
+   #}
+
+if __name__ == '__main__':
+    try:
+        generate_lnurl(float(sys.argv[1]))
+    except KeyboardInterrupt:
+        sys.exit('Manually Interrupted')
