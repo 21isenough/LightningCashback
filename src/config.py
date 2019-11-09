4d50e959c97753097aab3ab6d4d16114a73c9d8a
21isenough <39227503+21isenough@users.noreply.github.com>
2019-11-09 15:45:04 +0000

    add option for price API

--- src/config.py
+++ src/config.py
@@ -1,4 +1,7 @@
-## set user and password for lntxbot
+## Set user and password for lntxbot
 USER = '#####'
 PASS = '#####'
 APIURL = 'https://lntxbot.alhur.es/generatelnurlwithdraw'
+
+## API for the price request
+PRICEAPI = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCEUR'
