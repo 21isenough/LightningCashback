c8cd082a4fb0400660ea337f1fb9903b06db8ce5
21isenough <39227503+21isenough@users.noreply.github.com>
2019-11-09 19:42:32 +0000

    add LNURL API endpoint

---
+++ src/qrapi.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, request
+from escpos.printer import Usb
+
+import requests, json, sys
+
+from config import *
+
+app = Flask(__name__)
+
+@app.route('/lnurl', methods=['POST'])
+def generate_lnurl():
+    request_data = request.get_json()
+
+    p = Usb(0x0483, 0x5720, 0)
+
+    price_request = requests.get(PRICEAPI)
+    price_json = json.loads(price_request.text)
+    price = price_json['last']
+
+    satspereuro = round((1 / price) * 100000000)
+    satamount = round(satspereuro * float(request_data['euro']))
+
+    print("Sats per Euro = " + str(satspereuro))
+    print("Sats Total = " + str(satamount) + '\n')
+
+    data = {
+            'satoshis': str(round(satamount)),
+    }
+
+    r = requests.post(
+        APIURL,
+        auth=(USER, PASS),
+        data=json.dumps(data),
+        )
+
+    jsondata = json.loads(r.text)
+    response = json.dumps({'satoshis': satamount, 'lnurl': jsondata['lnurl']})
+
+    p.qr(jsondata['lnurl'],ec=0,size=10)
+    p.cut()
+
+    return "QR Code printed"
+    #return response
+
+app.run(port=5000)
